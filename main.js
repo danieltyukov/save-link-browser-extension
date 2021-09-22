1913e6f54e39b129ca05c60aa072b2c38f5cf6fd
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-09-22 10:25:16 +0300

    finished for testing purposes

--- main.js
+++ main.js
@@ -1,7 +1,54 @@
 let mySaves = [];
-const input = document.getElementById('input');
-const inputButton = document.getElementById('input-button');
-const deleteButton = document.getElementById('delete-button');
-const tabButton = document.getElementById('tab-button');
-const list = document.getElementById('list');
-const localSaves = JSON.parse(localStorage.getItem('saves'));
+const input = document.getElementById("input");
+const inputButton = document.getElementById("input-button");
+const deleteButton = document.getElementById("delete-button");
+const tabButton = document.getElementById("tab-button");
+const list = document.getElementById("list");
+const localSaves = JSON.parse(localStorage.getItem("mySaves"));
+
+if (localSaves) {
+  mySaves = localSaves;
+  render(mySaves);
+}
+
+tabButton.addEventListener("click", function () {
+  edge.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    function (tabs) {
+      mySaves.push(tabs[0].url);
+      localStorage.setItem("mySaves", JSON.stringify(mySaves));
+      render(mySaves);
+    }
+  );
+});
+
+function redner(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `;
+  }
+  list.innerHTML = listItems;
+}
+
+deleteButton.addEventListener("dblclick", function () {
+  localStorage.clear();
+  mySaves = [];
+  render(mySaves);
+});
+
+
+inputButton.addEventListener("click", function () {
+    mySaves.push(input.value);
+    input.value = ""
+    localStorage.setItem("mySaves", JSON.stringify(mySaves));
+    redner(mySaves);
+});
